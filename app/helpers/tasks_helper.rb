83bcec73355430eb2940f64c33ca19ae3ba23ab0
rayyiu <rayyiu66@gmail.com>
2023-04-26 15:46:59 -0700

    added basic date filtering logic, combination of ids for recurring tasks

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,6 +1,27 @@
 module TasksHelper
   def search_tasks_by_params
+    # non_recurring_tasks filtering
     query_params = {}
     query_params[:task_date] = @task_date_param
+    non_recurring_tasks = Task.where(query_params)
+
+    # Recurring Tasks filtering unimplemented
+    recurring_query_params = {}
+    selected_recurring_tasks = RecurringTask.where(recurring_query_params)
+
+    # date filter for recurring tasks
+    recurring_tasks = selected_recurring_tasks.map do |recurring_task|
+      if recurring_tasks.calculate_recurrence_dates_based_on_date(Date.parse(@task_date_param))
+        recurring_task
+      end
+    end
+
+    #combining ids
+    task_ids = (
+      non_recurring_tasks.pluck(:id) +
+      recurring_tasks.compact.pluck(:id)
+    ).uniq - completed_recurring_task_ids
+
+    @tasks = Task.where(id: task_ids)
   end
 end
