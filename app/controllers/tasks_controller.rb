8f973ad25fad98791a48e6ae63feb6fb490d8a18
rayyiu <rayyiu66@gmail.com>
2023-06-06 19:44:11 -0700

    recurring tasks create action no longer renders errors

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,9 +21,9 @@ class TasksController < ApplicationController
     #   ToDoList.new(formatted_to_do_list_params)
     # end
     @task = if task_params[:task_type_selection] == 'recurring'
-              RecurringTask.new(task_params)
+              RecurringTask.new(formatted_task_params)
             else
-              Task.new(task_params)
+              Task.new(formatted_task_params)
             end
 
     if @task.save
@@ -77,6 +77,33 @@ class TasksController < ApplicationController
                                  :repeat_on_0, :repeat_on_1, :repeat_on_2, :repeat_on_3, :repeat_on_4, :repeat_on_5, :repeat_on_6,
                                  :recurrence_start_date,
                                  :recurrence_end_date, :task_type_selection)
+  end
+
+  def formatted_task_params
+    if task_params[:task_type_selection] == 'recurring'
+      task_params.slice(*RecurringTask.column_names).merge(repeat_on: construct_repeat_on_column_content)
+    else
+      task_params.slice(*Task.column_names)
+    end
+  end
+
+  def construct_repeat_on_column_content
+    result_repeat_on_field = []
+    repeat_on_fields = %i[
+      repeat_on_0
+      repeat_on_1
+      repeat_on_2
+      repeat_on_3
+      repeat_on_4
+      repeat_on_5
+      repeat_on_6
+    ]
+
+    repeat_on_fields.each_with_index do |field_symbol, index|
+      result_repeat_on_field << index if task_params[field_symbol] == 'true'
+    end
+
+    result_repeat_on_field
   end
 
   VALID_ACTIONS = %w[new edit]
