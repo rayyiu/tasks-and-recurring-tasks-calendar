18d4b692d408381d40e6ee68ef747b252c7fca5c
rayyiu <rayyiu66@gmail.com>
2023-07-27 19:13:14 -0700

    basic calendar added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'calendar/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   resources :tasks, controller: :tasks do
     patch 'mark_as_completed', on: :member
   end
+
+  get '/calendar', to: 'calendar#index'
 
   # get :tasks, controller: :tasks
   get :index_search, controller: :tasks
